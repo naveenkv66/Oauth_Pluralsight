181ff19b9a1cb575c9a26a380199bfd8349984d6
Naveenkumar Venu <naveenkumar.v@falainacloud.com>
2020-07-31 22:45:03 +0530

    IdentityServer QuickStart Project added with UI

---
+++ src/Marvin.IDP/Quickstart/TestUsers.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "9ca1976d-8725-464f-85d4-2c88cc7f2017",
+                        Username = "frank",
+                        Password = "frank",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.GivenName, "frank"),                           
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                            
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "1059c763-988f-4096-9379-30785e2570b6",
+                        Username = "Claire",
+                        Password = "Claire",
+                        Claims =
+                        {
+                             new Claim(JwtClaimTypes.GivenName, "Claire"),
+                            new Claim(JwtClaimTypes.FamilyName, "Underwood"),
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
